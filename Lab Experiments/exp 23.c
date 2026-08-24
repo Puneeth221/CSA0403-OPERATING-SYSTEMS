8dc90a682d4033e001ca6a5342a4d4be479c6ac4
Puneeth221 <puneethgopu04@gmail.com>
2026-08-24 10:46:26 +0530

    Create exp 23.c

---
+++ Lab Experiments/exp 23.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int b[10], p[10], nb, np;
+    int i, j;
+
+    printf("Enter number of blocks: ");
+    scanf("%d", &nb);
+
+    printf("Enter block sizes:\n");
+    for(i = 0; i < nb; i++)
+        scanf("%d", &b[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d", &np);
+
+    printf("Enter process sizes:\n");
+    for(i = 0; i < np; i++)
+        scanf("%d", &p[i]);
+
+    printf("\nProcess\tSize\tBlock\n");
+
+    for(i = 0; i < np; i++)
+    {
+        for(j = 0; j < nb; j++)
+        {
+            if(b[j] >= p[i])
+            {
+                printf("P%d\t%d\tB%d\n", i + 1, p[i], j + 1);
+                b[j] -= p[i];
+                break;
+            }
+        }
+
+        if(j == nb)
+            printf("P%d\t%d\tNot Allocated\n", i + 1, p[i]);
+    }
+
+    return 0;
+}
